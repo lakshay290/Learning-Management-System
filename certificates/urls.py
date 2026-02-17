f5c9300092b1df8f056e6889b98e106fcb550f23
Lakshay Bishnoi <150051666+lakshay290@users.noreply.github.com>
2026-02-18 01:10:05 +0530

    Delete certificates directory

--- certificates/urls.py
+++
@@ -1,6 +0,0 @@
-
-from django.urls import path
-from . import views
-urlpatterns=[
-    path('generate/<int:id>/',views.generate_certificate),
-]
--- certificates/views.py
+++
@@ -1,6 +0,0 @@
-
-from django.shortcuts import render,get_object_or_404
-from courses.models import Enrollment
-def generate_certificate(request,id):
-    enroll=get_object_or_404(Enrollment,id=id)
-    return render(request,'certificate.html',{'enroll':enroll})
