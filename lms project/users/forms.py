086571ea7e2b215cdcf4e3d85966e7e520411a67
Lakshay Bishnoi <150051666+lakshay290@users.noreply.github.com>
2026-02-18 11:40:50 +0530

    Delete lms project directory

--- lms project/users/forms.py
+++
@@ -1,7 +0,0 @@
-
-from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser
-class RegisterForm(UserCreationForm):
-    class Meta:
-        model = CustomUser
-        fields = ['username','password1','password2','is_teacher']
